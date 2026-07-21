fcd7dc6c86af89205dc7c8dbe7a449c392771fd2
Nisarga S <nisargasgowda17@gmail.com>
2026-07-21 09:38:38 +0530

    Update Menu.js

--- backend/models/Menu.js
+++ backend/models/Menu.js
@@ -1,1 +1,11 @@
+const mongoose = require("mongoose");
 
+const menuSchema = new mongoose.Schema({
+  name: String,
+  price: Number,
+  image: String,
+  category: String,
+  restaurant: String
+});
+
+module.exports = mongoose.model("Menu", menuSchema);
